e7e1a264fda06700ca3b8d07865f35073a2b1775
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-04-05 14:13:16 +0530

    Initial Commit

---
+++ Favourite Athlete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Favourite Athlete
+//
+//  Created by student on 05/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
